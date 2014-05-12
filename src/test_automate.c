14a2bccc202dbee196490c19627b30c3c9c46abc
Baptiste <boruezabal@gmail.com>
2014-05-12 22:48:11 +0200

    [Test] Modification de test_automate pour tester miroir => ALL is done !

--- src/test_automate.c
+++ src/test_automate.c
@@ -18,10 +18,40 @@
  *    along with this Library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "automate.h"
 #include "outils.h"
 
 int main(){
-	A_FAIRE_RETURN(0);
+	Automate* automate = creer_automate();
+
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		ajouter_etat(automate, i);
+	}
+
+	ajouter_etat_initial(automate, 0);
+	ajouter_transition(automate, 0, 'a', 1);
+	ajouter_transition(automate, 0, 'b', 0);
+	ajouter_transition(automate, 0, 'c', 2);
+	ajouter_transition(automate, 1, 'b', 2);
+	ajouter_transition(automate, 1, 'c', 1);
+	ajouter_transition(automate, 2, 'a', 3);
+	ajouter_transition(automate, 3, 'b', 4);
+	ajouter_transition(automate, 3, 'c', 1);
+	ajouter_transition(automate, 4, 'a', 4);
+	ajouter_etat_final(automate, 4);
+
+	Automate* reverse = miroir(automate);
+	
+	printf("Automate....\n");
+	print_automate(automate);
+	printf("Miroir......\n");
+	print_automate(reverse);
+
+	exit(1);
 }
